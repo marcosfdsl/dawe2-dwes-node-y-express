3f5ff2e03f344f01ee2c1017dee4c2ee6075f4c2
MarcosFdS <marcos.fernandez@live.u-tad.com>
2023-12-01 12:17:01 +0100

    ca4

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/nodeyexpress/browser/index.html'));
+app.use(express.static('./dist/nodeyexpress/browser'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/nodeyexpress/browser/index.html'});
+  res.sendFile('index.html', {root: 'dist/nodeyexpress/browser/'});
 });
 
 const port = process.env.PORT || 3000;
